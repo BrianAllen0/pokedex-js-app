5f87d6c54e3954bbe44a0a3ff9ae92bbeef91075
BrianAllen0 <123843772+BrianAllen0@users.noreply.github.com>
2023-02-20 18:48:40 -0800

    Added "pokemonRepository" IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,30 @@
-let pokemonList = [];
-let bulbasaurObject = {
-    name: 'Bulbasaur',
-    height: 0.7,
-    types: ['Grass', 'Poison']
-}
-let charmanderObject = {
-    name: 'Charmander',
-    height: 0.6,
-    types: ['Fire']
-}
-let squirtleObject = {
-    name: 'Squirtle',
-    height: 0.5,
-    types: ['Water']
-}
-pokemonList.push(bulbasaurObject, charmanderObject, squirtleObject);
+let pokemonRepository = (function () {
+    let pokemonList = [];
+    let bulbasaurObject = {
+        name: 'Bulbasaur',
+        height: 0.7,
+        types: ['Grass', 'Poison']
+    }
+    let charmanderObject = {
+        name: 'Charmander',
+        height: 0.6,
+        types: ['Fire']
+    }
+    let squirtleObject = {
+        name: 'Squirtle',
+        height: 0.5,
+        types: ['Water']
+    }
+    pokemonList.push(bulbasaurObject, charmanderObject, squirtleObject);
+    
+    function getAll() {
+        return pokemonList;
+    }
+    function add(newPokemon) {
+        pokemonList.push(newPokemon);
+    }
+})();
+
 
 pokemonList.forEach(function(pokemon){
     document.write('Name: ' + pokemon.name + '<br>');
