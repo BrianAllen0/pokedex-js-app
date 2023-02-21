1793e58226e299ca3fdf7e69923eec48e90bbbb5
BrianAllen0 <123843772+BrianAllen0@users.noreply.github.com>
2023-02-20 16:59:33 -0800

    Added pokemon list and initial 3 pokemon with simple output

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
-alert('Hello world');
-let favoriteFood = 'Chicken';
-document.write(favoriteFood);
+let pokemonList = [];
+let bulbasaurObject = {
+    name: 'Bulbasaur',
+    height: 0.7,
+    types: ['Grass', 'Poison']
+}
+let charmanderObject = {
+    name: 'Charmander',
+    height: 0.6,
+    types: ['Fire']
+}
+let squirtleObject = {
+    name: 'Squirtle',
+    height: 0.5,
+    types: ['Water']
+}
+pokemonList.push(bulbasaurObject, charmanderObject, squirtleObject);
+for (let index = 0; index < pokemonList.length; index++) {
+    document.write('Name: ' + pokemonList[index].name + '<br>');
+    document.write('Height: ' + pokemonList[index].height + ' m' + '<br>');
+    document.write('Type: ' + pokemonList[index].types + '<br>');
+    document.write("<br>");
+}
