669ba5216a529d1b88e5f3f5c88a871ed73d03cd
BrianAllen0 <123843772+BrianAllen0@users.noreply.github.com>
2023-02-20 18:27:26 -0800

    Refactored for loop to a foreach loop

--- js/scripts.js
+++ js/scripts.js
@@ -15,14 +15,15 @@ let squirtleObject = {
     types: ['Water']
 }
 pokemonList.push(bulbasaurObject, charmanderObject, squirtleObject);
-for (let index = 0; index < pokemonList.length; index++) {
-    document.write('Name: ' + pokemonList[index].name + '<br>');
+
+pokemonList.forEach(function(pokemon){
+    document.write('Name: ' + pokemon.name + '<br>');
     let heightComment = "";
-    if(pokemonList[index].height >= 0.6)
+    if(pokemon.height >= 0.6)
         heightComment = " (Wow, that’s big!)";
     else
         heightComment = " (Wow, that’s small!)";
-    document.write('Height: ' + pokemonList[index].height + ' m' + heightComment + '<br>');
-    document.write('Type: ' + pokemonList[index].types + '<br>');
+    document.write('Height: ' + pokemon.height + ' m' + heightComment + '<br>');
+    document.write('Type: ' + pokemon.types + '<br>');
     document.write("<br>");
-}
+});
