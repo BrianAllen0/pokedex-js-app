c8c4ef4634e5c1c1be52943a46023e4b335e81e5
BrianAllen0 <123843772+BrianAllen0@users.noreply.github.com>
2023-03-01 11:48:38 -0800

    Added button event listener to log pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,28 @@
 let pokemonRepository = (function () {
     let pokemonList = [];
     pokemonList.push({name: 'Bulbasaur', height: 0.7, types: ['Grass', 'Poison']}, {name: 'Charmander', height: 0.6, types: ['Fire']}, {name: 'Squirtle', height: 0.5, types: ['Water']});
+    
     function getAll() {
         return pokemonList;
     }
+
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    }
+
     function addListItem(pokemon) {
         let pokemonUnorderedList = document.querySelector('.pokemon-list');
         let listItem = document.createElement('li');
         let button = document.createElement('button');
+        
+        button.addEventListener('click', function(){showDetails(pokemon);})
         button.innerText = pokemon.name;
         button.classList.add('pokemon-button')
+        
         listItem.appendChild(button);
         pokemonUnorderedList.appendChild(listItem);
     }
+
     return {
         getAll: getAll,
         addListItem: addListItem
