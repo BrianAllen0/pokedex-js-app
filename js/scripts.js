dffeeb6aa2a83b63a3cbc70a63de716474bc6261
BrianAllen0 <123843772+BrianAllen0@users.noreply.github.com>
2023-02-28 19:37:48 -0800

    Updated pokemon display loop
    Now display loops uses query selector, adding created listitems to an unordered list

--- js/scripts.js
+++ js/scripts.js
@@ -29,14 +29,12 @@ let pokemonRepository = (function () {
     }
 })();
 
+let pokemonList = document.querySelector('.pokemon-list');
 pokemonRepository.getAll().forEach(function(pokemon){
-    document.write('Name: ' + pokemon.name + '<br>');
-    let heightComment = "";
-    if(pokemon.height >= 0.6)
-        heightComment = " (Wow, that’s big!)";
-    else
-        heightComment = " (Wow, that’s small!)";
-    document.write('Height: ' + pokemon.height + ' m' + heightComment + '<br>');
-    document.write('Type: ' + pokemon.types + '<br>');
-    document.write("<br>");
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-button')
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
 });
