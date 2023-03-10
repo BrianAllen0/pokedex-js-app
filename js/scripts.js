bf22129b1808443ea026a208f8e6e927b20b8edb
BrianAllen0 <123843772+BrianAllen0@users.noreply.github.com>
2023-03-09 19:53:13 -0800

    Removed debug elements for designing style

--- js/scripts.js
+++ js/scripts.js
@@ -61,8 +61,6 @@ let pokemonRepository = (function () {
     }
 })();
 
-
-
 pokemonRepository.loadList().then(function() {
     pokemonRepository.getAll().forEach(function(pokemon) {
         pokemonRepository.addListItem(pokemon);
